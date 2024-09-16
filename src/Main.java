25046be74138ac08455ceee301954eecdd053271
4epper <anand.arjun196@gmail.com>
2024-09-16 17:14:43 -0400

    test Terminal

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 package src;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello gay!");
+        boolean running = true;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello. Welcome to simple chat! type bitch.");
+        while (running) {
+            System.out.print(">> ");
+            String input = scanner.nextLine(); // Read the input as a string
+            if (input.equals("Q")) {
+                System.out.print("Scheduler is terminated.");
+                running = false;
+            }
+        }
     }
 }
