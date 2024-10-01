b6a0cbce11ffe37c34a904d9c6a99a99cf1fac9d
nohac14 <kbac97@gmail.com>
2024-09-30 21:58:14 -0400

    WE ARE DONNEEEEEEE

--- clinic/RunProject1.java
+++ clinic/RunProject1.java
@@ -1,6 +1,21 @@
 package clinic;
 
+/**
+ * The RunProject1 class is the main entry point for the clinic scheduling application.
+ * It creates an instance of the Scheduler class and initiates the scheduling system.
+ * The Scheduler class handles commands such as scheduling, canceling, and rescheduling appointments.
+ * This class is responsible for starting the program.
+ *
+ * @author Jonas Lazebnik, Arjun Anand
+ */
 public class RunProject1 {
+
+    /**
+     * The main method that starts the clinic scheduling application by creating
+     * a new instance of the Scheduler and invoking its run method.
+     *
+     * @param args command-line arguments (not used in this application)
+     */
     public static void main(String[] args) {
         new Scheduler().run();
     }
